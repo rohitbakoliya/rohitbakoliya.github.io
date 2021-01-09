2145d595a9e45774487afaaa6296c6393604d1bf
rohitbakoliya <bakoliyarohit00@gmail.com>
2021-01-09 16:52:11 +0530

    polyfill added for intersection observer

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,3 +6,10 @@ require('typeface-mulish');
 require('./src/styles/prism-purpleking-dark.css');
 require('prismjs/plugins/line-numbers/prism-line-numbers.css');
 require('prismjs/plugins/command-line/prism-command-line.css');
+
+// polyfill for intersection Observer API
+export const onClientEntry = async () => {
+    if (typeof IntersectionObserver === `undefined`) {
+        await import(`intersection-observer`);
+    }
+};
