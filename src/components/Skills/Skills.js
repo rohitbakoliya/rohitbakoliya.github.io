ffd6ac14ac4354a2fd1eb26b57c89c4334b02817
Rohit Bakoliya <bakoliyarohit00@gmail.com>
2025-04-12 16:02:08 +0000

    cleaner skill section

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { useScreenClass, Hidden } from 'react-grid-system';
+import React from 'react';
+import { Hidden } from 'react-grid-system';
 import { ParallaxProvider } from 'react-scroll-parallax';
-
-import Button from '../common/Button';
 
 import IconsLib from '../common/IconsLib';
 import PageHeader from '../common/PageHeader';
@@ -14,7 +12,7 @@ const WordClouds = () => {
         <SkillsWordClouds y={['-350px', '-150px']} speed={-10} aria-hidden="true">
             <p style={{ top: 50, left: '100%', fontSize: 23 }}>OpenAI</p>
             <p style={{ top: 0, left: 0, fontSize: 25 }}>ZooKeeper</p>
-            <p style={{ top: 200, left: -60, fontSize: 14 }}>etcd</p>
+            <p style={{ top: 200, left: -60, fontSize: 14 }}>Consul</p>
             <p style={{ top: '30%', left: '35%', fontSize: 18 }}>gRPC</p>
             <p style={{ top: 40, left: '75%', fontSize: 12 }}>Comdb2</p>
             <p style={{ top: 380, left: '85%', fontSize: 18 }}>Nginx</p>
@@ -25,39 +23,37 @@ const WordClouds = () => {
     );
 };
 const Skills = () => {
+    /* Icons are currently limited, so this is not needed
+    
     const screenClass = useScreenClass();
+    
     const [showAllLang, setShowAllLang] = useState(screenClass !== `xs`);
-    const [showAllDb, setShowAllDb] = useState(screenClass !== `xs`);
-    const [showAllInfra, setShowAllInfra] = useState(screenClass !== `xs`);
 
     useEffect(() => {
-        if (screenClass === 'xs') {
-            setShowAllLang(false);
-            setShowAllDb(false);
-            setShowAllInfra(false);
-        } else {
-            setShowAllLang(true);
-            setShowAllDb(true);
-            setShowAllInfra(true);
-        }
+        const screenFlag = screenClass !== 'xs';
+        setShowAllLang(screenFlag);
     }, [screenClass]);
+    
+    <SkillsIconsWrapper showAll={showAllLang} collapseHeight="450px">
+        <IconsLib Cplusplus Python Go Javascript ReactJs Graphql Fastapi NodeDotJs />
+        {!showAllLang && (
+            <Button onClick={() => setShowAllLang(true)} className="showall__button">
+                Show all
+            </Button>
+        )} 
+    </SkillsIconsWrapper>
+    */
 
     return (
         <>
             <SkillsWrapper id="skills">
                 <ParallaxProvider>
-                    <PageHeader>My Skillsets</PageHeader>
+                    <PageHeader>My Skillset</PageHeader>
 
                     <SubHeading>Languages & Frameworks</SubHeading>
 
-                    <SkillsIconsWrapper showAll={showAllLang} collapseHeight="450px">
+                    <SkillsIconsWrapper showAll={true} collapseHeight="450px">
                         <IconsLib Cplusplus Python Go Javascript ReactJs Graphql Fastapi NodeDotJs />
-
-                        {!showAllLang && (
-                            <Button onClick={() => setShowAllLang(true)} className="showall__button">
-                                Show all
-                            </Button>
-                        )}
                     </SkillsIconsWrapper>
 
                     <Hidden md xs sm>
@@ -66,26 +62,14 @@ const Skills = () => {
 
                     <SubHeading>Data & Messaging Systems</SubHeading>
 
-                    <SkillsIconsWrapper showAll={showAllDb} collapseHeight="450px">
+                    <SkillsIconsWrapper showAll={true} collapseHeight="450px">
                         <IconsLib Postgresql Mysql Mongodb ApacheCassandra Redis Apachekafka Rabbitmq Celery />
-
-                        {!showAllDb && (
-                            <Button onClick={() => setShowAllDb(true)} className="showall__button">
-                                Show all
-                            </Button>
-                        )}
                     </SkillsIconsWrapper>
 
                     <SubHeading>Cloud, Infrastructure & Observability</SubHeading>
 
-                    <SkillsIconsWrapper showAll={showAllInfra} collapseHeight="450px">
+                    <SkillsIconsWrapper showAll={true} collapseHeight="450px">
                         <IconsLib Amazonaws Docker Kubernetes Terraform Grafana Datadog Sentry />
-
-                        {!showAllInfra && (
-                            <Button onClick={() => setShowAllInfra(true)} className="showall__button">
-                                Show all
-                            </Button>
-                        )}
                     </SkillsIconsWrapper>
                 </ParallaxProvider>
             </SkillsWrapper>
